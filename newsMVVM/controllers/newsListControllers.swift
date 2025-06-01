c03aeb417a101609e0cb61c8975451192d437345
vishnuprasad <vishnup@yeswearemad.com>
2025-06-01 11:55:24 +0530

    cleanup

--- newsMVVM/controllers/newsListControllers.swift
+++ newsMVVM/controllers/newsListControllers.swift
@@ -22,18 +22,14 @@ class newsListTableVIewController : UITableViewController{
                 DispatchQueue.main.async{
                     self.tableView.reloadData()
                 }
-
             }
-            
         }
-      
     }
     override func numberOfSections(in tableVIew : UITableView) -> Int{
         return self.articleListVm == nil ? 0 : self.articleListVm.numberofSection
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.articleListVm == nil ? 0 : self.articleListVm.numberofROwsinSection(section)
-
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else{
--- newsMVVM/services/webservices.swift
+++ newsMVVM/services/webservices.swift
@@ -7,19 +7,19 @@
 import Foundation
 class Webservices {
     func getArticles(url : URL ,  completion:@escaping (([Article])?)->()) {
-        URLSession.shared.dataTask(with: url) { data, response , error in
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        URLSession.shared.dataTask(with: request) { data, response , error in
             if let err = error {
-            print(err)
+                print(err)
                 completion(nil)
             } else if let data = data {
-                
                 let articlesList = try? JSONDecoder().decode(ArticleList.self, from: data)
                 if let articlesList = articlesList{
                     completion(articlesList.articles)
                 }
-                print(articlesList?.articles)
             }
-            
         }.resume()
     }
 }
